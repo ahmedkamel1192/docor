3e50880bb5f6ce6efafa2ea06c2938968bd742df
ahmedkamel <ahmed.kamel1192@gmail.com>
2018-08-06 19:00:43 +0200

    push notification test

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,6 +9,7 @@ class NotificationController extends Controller
 
      public function sendNotification()
      {
+         dd(request('deviceToken'));
          PushNotification::app('superDoctorAndroid')
              ->to(request('deviceToken'))
              ->send('Hello World, i`m a push message');

9ea382ed67b946db767e43126c0d6d0ed85d3206
ahmedkamel <ahmed.kamel1192@gmail.com>
2018-08-07 20:56:12 +0200

    add admin and lte control panel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ Route::get('/', function () {
 
     return view('welcome');
 });
+
+Auth::routes();
+Route::group(['middleware' => ['checkIfAdmin']], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+
+});
+

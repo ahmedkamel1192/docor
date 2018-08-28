29417f9f2cbbdf1261f88a0a2a44fa1b07f7f2f0
ahmedkamel <ahmed.kamel1192@gmail.com>
2018-08-29 01:34:01 +0200

    allow access

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
-        'allow_origin' => \App\Http\Middleware\Cors::class,
+  // 'allow_origin' => \App\Http\Middleware\Cors::class,
     ];
 }

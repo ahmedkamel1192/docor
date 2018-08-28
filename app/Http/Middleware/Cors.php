114a8d0dc58fdb9fac7c85c2b9a19c6a25931a41
ahmedkamel <ahmed.kamel1192@gmail.com>
2018-08-29 01:11:42 +0200

    allow access

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,8 +17,10 @@ class Cors
     public function handle($request, Closure $next)
         {
             return $next($request)
-               // ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+               ->header('Access-Control-Allow-Origin', '*')
+               ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+              ->header('Access-Control-Allow-Headers: Origin, Content-Type,Authorizations')
+ 
+              ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
         }
 }

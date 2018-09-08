fa442fbf6760fcc4317f6beaaf9014ac49c65ec2
ahmedkamel <ahmed.kamel1192@gmail.com>
2018-09-08 17:36:49 +0200

    test

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -11,8 +11,11 @@ class NotificationController extends Controller
 
      public function requestDoctor()
      {
+
          $doctor_id = request('doctor_id');
          $doctor = User::find($doctor_id);
+         return response()->json(['message'=>'true','data' =>$doctor->device_token ], 200);
+
          $current_user = auth()->user();
          \PushNotification::app('superDoctorAndroid')
              ->to($doctor->device_token)
